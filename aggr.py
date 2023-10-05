f1871edc293349b4b12c2865fe9fa5049907d2dc
Keerthi.P <117421912+Keerthi2000kithu@users.noreply.github.com>
2023-10-05 14:04:18 +0530

    Add files via upload

---
+++ aggr.py
@@ -0,0 +1,11 @@
+print ("demo of  basic data types: Numbers")
+x=3
+y=2.5
+print("x=",x)
+print("y=",y)
+print(" data type of variable x:", type(x))
+print(" data type of variable y:", type(y))
+print("addition :",x+y)
+print("subtraction :",x-y)
+print("multiplication :",x*y)
+print("exponentition :",x**2)
