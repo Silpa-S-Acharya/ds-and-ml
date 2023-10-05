764242eace4feea2150a776d1e83c821e6a4458e
Keerthi.P <117421912+Keerthi2000kithu@users.noreply.github.com>
2023-10-05 15:14:54 +0530

    Add files via upload

---
+++ arth.py
@@ -0,0 +1,11 @@
+print ("demo of  basic data types: Boolean")
+t=True
+f=False
+print("t=",t)
+print("f=",f)
+print(" data type of variable t:", type(t))
+print(" data type of variable f:", type(f))
+print("logical AND operation :",t and f)
+print("logical OR operation :",t or f)
+print("logical NOT operation:",not t)
+print("logical XOR operation :",t != f)
